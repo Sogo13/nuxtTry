9830720b959c0c73d588a9cd6c558b039e02ddad
Sogo13 <Backin90@yandex.ru>
2024-03-05 20:57:34 +0300

    nuxt

---
+++ components/DraggableList.vue
@@ -0,0 +1,29 @@
+<template>
+  <draggable v-model="items">
+    <div v-for="(item, index) in items" :key="index" class="item">{{ item }}</div>
+  </draggable>
+</template>
+
+<script>
+import draggable from 'vuedraggable';
+
+export default {
+  components: {
+    draggable,
+  },
+  data() {
+    return {
+      items: ['Item 1', 'Item 2', 'Item 3'],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.item {
+  background-color: #f0f0f0;
+  border: 1px solid #ccc;
+  padding: 10px;
+  margin: 5px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Drag and Drop Example</h1>
+    <DraggableList />
+  </div>
+</template>
+
+<script>
+import DraggableList from '@/components/DraggableList.vue';
+export default {
+  name: 'IndexPage',
+  components: {
+    DraggableList,
+  },
+}
+</script>
